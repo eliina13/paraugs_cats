eb3fc0324624eb51cc466558b742e950ef2ff7ce
eliina13 <92846329+eliina13@users.noreply.github.com>
2024-04-15 15:40:00 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+//console.log(1)
+function sutitZinu()
+{
+    console.log('sutitZinu()darbojas');
+    let zina = document.querySelector('zina');
+    let zinas = document.querySelector('cataZina');
+    zinas.innerHTML = zinas.innerHTML+ '<br>'+zina.value;
+
+}
